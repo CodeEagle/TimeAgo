bba97d98b3c2347f8511097ac89ce4f2c31e2e03
CodeEagle <stasura@hotmail.com>
2016-11-08 17:01:48 +0800

    optimize

--- Source/TimeAgo.swift
+++ Source/TimeAgo.swift
@@ -11,9 +11,13 @@ extension String {
 open class TimeAgo: NSObject {
 
 	open class Words: NSObject {
+        var yearAgo = " year ago"
 		var yearsAgo = " year(s) ago"
+        var monthAgo = " month ago"
 		var monthsAgo = " month(s) ago"
+        var weekAgo = " week ago"
 		var weeksAgo = " week(s) ago"
+        var dayAgo = " day ago"
 		var daysAgo = " day(s) ago"
 		var hoursAgo = " hours ago"
 		var anHourAgo = "An hour ago"
@@ -24,12 +28,16 @@ open class TimeAgo: NSObject {
 
 		public override init() { super.init() }
 
-		public convenience init(yearsAgo: String, monthsAgo: String, weeksAgo: String, daysAgo: String, hoursAgo: String, anHourAgo: String, minutesAgo: String, anMinuteAgo: String, secondsAgo: String, justNow: String) {
+        public convenience init(yearsAgo: String, yearAgo: String, monthsAgo: String, monthAgo: String, weeksAgo: String, weekAgo: String, daysAgo: String, dayAgo: String, hoursAgo: String, anHourAgo: String, minutesAgo: String, anMinuteAgo: String, secondsAgo: String, justNow: String) {
 			self.init()
 			self.yearsAgo = yearsAgo
+            self.yearAgo = yearAgo
 			self.monthsAgo = monthsAgo
+            self.monthAgo = monthAgo
 			self.weeksAgo = weeksAgo
+            self.weekAgo = weekAgo
 			self.daysAgo = daysAgo
+            self.dayAgo = dayAgo
 			self.hoursAgo = hoursAgo
 			self.anHourAgo = anHourAgo
 			self.minutesAgo = minutesAgo
@@ -43,9 +51,9 @@ open class TimeAgo: NSObject {
 
 	fileprivate override init() {
 		super.init()
-		let zhhans = Words(yearsAgo: "年前", monthsAgo: "个月前", weeksAgo: "星期前", daysAgo: "天前", hoursAgo: "小时前", anHourAgo: "1小时前", minutesAgo: "分钟前", anMinuteAgo: "1分钟前", secondsAgo: "秒钟前", justNow: "刚刚")
+		let zhhans = Words(yearsAgo: "年前", yearAgo: "年前", monthsAgo: "个月前", monthAgo: "个月前", weeksAgo: "星期前", weekAgo: "星期前", daysAgo: "天前", dayAgo: "天前", hoursAgo: "小时前", anHourAgo: "1小时前", minutesAgo: "分钟前", anMinuteAgo: "1分钟前", secondsAgo: "秒钟前", justNow: "刚刚")
 
-		let zhhant = Words(yearsAgo: "年前", monthsAgo: "個月前", weeksAgo: "星期前", daysAgo: "天前", hoursAgo: "小時前", anHourAgo: "1小時前", minutesAgo: "分鐘前", anMinuteAgo: "1分鐘前", secondsAgo: "秒鐘前", justNow: "剛剛")
+        let zhhant = Words(yearsAgo: "年前", yearAgo: "年前", monthsAgo: "個月前", monthAgo: "個月前", weeksAgo: "星期前", weekAgo: "星期前", daysAgo: "天前", dayAgo: "天前", hoursAgo: "小時前", anHourAgo: "1小時前", minutesAgo: "分鐘前", anMinuteAgo: "1分鐘前", secondsAgo: "秒鐘前", justNow: "剛剛")
 		let en = Words()
 		configureForCustomWords["zh-Hans"] = zhhans
 		configureForCustomWords["zh-Hant"] = zhhant
@@ -100,14 +108,12 @@ extension Date {
 
 		let config = TimeAgo.manager.config
 
-		if let y = components.year, y >= 1 { return "\(y)" + config.yearsAgo }
-		if let m = components.month, m >= 1 { return "\(m)" + config.monthsAgo }
-		if let w = components.weekOfYear, w >= 1 { return "\(w)" + config.weeksAgo }
-		if let d = components.day, d >= 1 { return "\(d)" + config.daysAgo }
-		if let h = components.hour, h >= 2 { return "\(h)" + config.hoursAgo }
-		if let h = components.hour, h >= 1 { return config.anHourAgo }
-		if let m = components.minute, m >= 2 { return "\(m)" + config.minutesAgo }
-		if let m = components.minute, m >= 1 { return config.anMinuteAgo }
+        if let y = components.year { return  "\(y)" + (y > 1 ? config.yearsAgo : config.yearAgo) }
+        if let m = components.month { return "\(m)" + (m > 1 ? config.monthsAgo : config.monthAgo) }
+		if let w = components.weekOfYear { return "\(w)" + (w > 1 ? config.weeksAgo : config.weekAgo) }
+		if let d = components.day { return "\(d)" + (d > 1 ? config.daysAgo : config.dayAgo) }
+        if let h = components.hour { return h > 1 ? ("\(h)" + config.hoursAgo) : config.anHourAgo }
+		if let m = components.minute{ return m > 1 ? ("\(m)" + config.minutesAgo) : config.anMinuteAgo }
 		if let s = components.second, s >= 3 { return "\(s)" + config.secondsAgo }
 		return config.justNow
 	}
